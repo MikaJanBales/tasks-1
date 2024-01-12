971ad54ce248b1e8620ac9c706dab878d7972a4f
MikaJanBales <114147741+MikaJanBales@users.noreply.github.com>
2024-01-12 15:42:07 +0300

    2

--- 1/main.go
+++ 1/main.go
@@ -1,5 +1,78 @@
 package main
 
-func main() {
+import "fmt"
 
+type Human struct {
+	Name    string
+	Address string
+	Phone   string
+}
+
+func (h *Human) SetName(name string) {
+	h.Name = name
+}
+
+func (h *Human) SetPhone(phone string) {
+	h.Phone = phone
+}
+
+func (h *Human) SetAddress(address string) {
+	h.Address = address
+}
+
+type Action struct {
+	Name       string
+	SecondName string
+	Address    string
+	H          Human
+	Human
+}
+
+func (a *Action) SetName(name string) {
+	a.Name = name
+}
+
+func main() {
+	action := Action{
+		Name:       "John",
+		SecondName: "Doe",
+		Address:    "qwerty",
+		Human: Human{
+			Name:    "Jane",
+			Address: "456",
+			Phone:   "+79991234578",
+		},
+		H: Human{
+			Name:    "Peter",
+			Address: "123",
+			Phone:   "+74561237845",
+		},
+	}
+
+	// при наличии одикаковых полей, приоритет у поля более верхнего уровня
+	fmt.Println(action.Name) // Jonn
+
+	// чтобы обратиться к полю встроенной структуре, нужно указать ее тип, затем поле
+	fmt.Println(action.Human.Name) // Jane
+
+	// если поле уникальное, то можно напрямую обращаться к нему из родительской структуры
+	fmt.Println(action.Phone) // +79991234578
+
+	// чтобы обратиться к полю типа структуры, нужно указать имя поля, затем необходимое поле вложенной структуры
+	fmt.Println(action.H.Name) // Peter
+
+	//
+
+	// методы встроенной структуры наследуются родительской
+	action.SetPhone("+712345")
+	fmt.Println(action.Phone) // +712345
+
+	// при наличии одинаковых методов, приоритет у метода структуры более верхнего уровня
+	action.SetName("new name")
+	fmt.Println(action.Name)       // new name
+	fmt.Println(action.Human.Name) // Jane
+
+	// чтобы вызвать этот метод у встроенной структуры, необходимо обратиться к ней через тип
+	action.Human.SetName("Ivan")
+	fmt.Println(action.Human.Name) // Ivan
 }
--- 2/main.go
+++ 2/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+func main() {
+	nums := []int{2, 4, 6, 8, 10}
+	wg := new(sync.WaitGroup)
+
+	for i, el := range nums {
+		wg.Add(1)
+		go func(i int, el int) {
+			defer wg.Done()
+			nums[i] = el * el
+		}(i, el)
+	}
+
+	wg.Wait()
+
+	fmt.Println(nums)
 }
