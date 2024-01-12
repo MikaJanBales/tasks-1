32adc51139f738e66a4693d9caafb5d68c653109
MikaJanBales <114147741+MikaJanBales@users.noreply.github.com>
2024-01-12 15:51:12 +0300

    3

--- 3/main.go
+++ 3/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+func main() {
+	nums := []int{2, 4, 6, 8, 10}
+	wg := new(sync.WaitGroup)
+	var res int
+
+	for _, el := range nums {
+		wg.Add(1)
+		go func(el int) {
+			defer wg.Done()
+			res += el * el
+		}(el)
+	}
+
+	wg.Wait()
+
+	fmt.Print(res)
 }
