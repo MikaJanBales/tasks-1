666d1f033b3a73e4d1bc2682cc9848c1b75dec98
MikaJanBales <114147741+MikaJanBales@users.noreply.github.com>
2024-01-15 16:51:16 +0300

    12

--- 12/main.go
+++ 12/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	arr := []string{"cat", "cat", "dog", "cat", "tree"}
+	fmt.Printf("set: %v\n", arr)
+
+	m := make(map[string]struct{})
+	res := make([]string, 0)
+
+	for _, v := range arr {
+		if _, ok := m[v]; !ok {
+			res = append(res, v)
+			m[v] = struct{}{}
+		}
+	}
+
+	fmt.Printf("subset: %v\n", res)
 }
