9bec8200e4d680aff2f11aee4281b79703b4dc08
MikaJanBales <114147741+MikaJanBales@users.noreply.github.com>
2024-01-15 17:09:58 +0300

    14

--- 14/main.go
+++ 14/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"reflect"
+)
 
+func main() {
+	ch := make(chan int)
+	arr := []any{"hi", 42, true, ch}
+	for _, v := range arr {
+		switch x := v.(type) {
+		case int:
+			fmt.Println("int:", x)
+		case string:
+			fmt.Println("string:", x)
+		case bool:
+			fmt.Println("bool:", x)
+		case chan int:
+			fmt.Println("channel for integer:", x)
+		default:
+			fmt.Println(reflect.TypeOf(v).String())
+		}
+	}
 }
