b0b8213d697d0a38277cd529426bf9e71263c092
MikaJanBales <114147741+MikaJanBales@users.noreply.github.com>
2024-01-16 20:42:12 +0300

    26 unique

--- 26/main.go
+++ 26/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func isUnique(str string) bool {
+	str = strings.ToLower(str)
+	m := make(map[rune]struct{})
+
+	for _, r := range str {
+		if _, ok := m[r]; ok {
+			return false
+		}
+		m[r] = struct{}{}
+	}
+
+	return true
+}
+
+func main() {
+	fmt.Printf("abcd: %v\n", isUnique("abcd"))
+	fmt.Printf("abCdefAaf: %v\n", isUnique("abCdefAaf"))
+	fmt.Printf("aabcd: %v\n", isUnique("aabcd"))
 }
