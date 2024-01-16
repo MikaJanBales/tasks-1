f386915386f410d69a5c83f12a31a1f6d3493791
MikaJanBales <114147741+MikaJanBales@users.noreply.github.com>
2024-01-16 16:25:15 +0300

    17 binary search

--- 17/main.go
+++ 17/main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
+	"golang.org/x/exp/constraints"
 )
 
-func binarySearch(list []int, item int, left int, right int) int {
+func binarySearch[T constraints.Ordered](list []T, item T, left int, right int) int {
 	if left > right {
 		return -1
 	}
@@ -22,10 +23,19 @@ func binarySearch(list []int, item int, left int, right int) int {
 
 func main() {
 	myList := []int{1, 3, 5, 7, 9, 13, 15, 17, 19, 20, 25, 34, 44, 45, 55, 56, 67, 78, 90, 100}
+	myFloatList := []float32{1.1, 1.6, 2.5, 4.5, 6.1, 7.6, 11.4, 15.5}
+	myStringList := []string{"ab", "ac", "b", "bcd", "fav", "fbd"}
 
-	fmt.Println(binarySearch(myList, 100, 0, len(myList)-1))
 	fmt.Println(binarySearch(myList, 1, 0, len(myList)-1))
-	fmt.Println(binarySearch(myList, 15, 0, len(myList)-1))
+	fmt.Println(binarySearch(myList, 45, 0, len(myList)-1))
 	fmt.Println(binarySearch(myList, 28, 0, len(myList)-1))
-	fmt.Println(binarySearch(myList, 0, 0, len(myList)-1))
+
+	fmt.Println(binarySearch(myFloatList, 1.6, 0, len(myFloatList)-1))
+	fmt.Println(binarySearch(myFloatList, 10, 0, len(myFloatList)-1))
+	fmt.Println(binarySearch(myFloatList, 15.5, 0, len(myFloatList)-1))
+
+	fmt.Println(binarySearch(myStringList, "arm", 0, len(myStringList)-1))
+	fmt.Println(binarySearch(myStringList, "ab", 0, len(myStringList)-1))
+	fmt.Println(binarySearch(myStringList, "fav", 0, len(myStringList)-1))
+
 }
