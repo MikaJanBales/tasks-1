0fea828c069b0cf5f24a7275be32040493ec69e9
MikaJanBales <114147741+MikaJanBales@users.noreply.github.com>
2024-01-16 20:25:43 +0300

    23 delete element in slice

--- 23/main.go
+++ 23/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+)
 
+func deleteElem[T any](arr []T, i int) []T {
+	return append(arr[:i], arr[i+1:]...)
+}
+
+func deleteElemNoOrder[T any](arr []T, i int) []T {
+	arr[i] = arr[len(arr)-1]
+	arr = arr[:len(arr)-1]
+	return arr[:len(arr)-1]
+}
+
+func main() {
+	// удаление элемента
+	a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Printf("      original slice: %v\n", a)
+	a = deleteElem(a, 2)
+	fmt.Printf("deleted 3-rd element: %v\n\n", a)
+
+	// удаление элемента без сохранения порядка
+	a = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Printf("      original slice: %v\n", a)
+	a = deleteElemNoOrder(a, 2)
+	fmt.Printf("deleted 3-rd element: %v (no order)\n\n", a)
 }
