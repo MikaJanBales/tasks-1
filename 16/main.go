0f487cdb60e21a9bc6914b03d14ff95d0a5fa269
MikaJanBales <114147741+MikaJanBales@users.noreply.github.com>
2024-01-16 11:27:31 +0300

    16 quick sorting

--- 16/main.go
+++ 16/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import "fmt"
 
+func quickSort(m []int, left int, right int) []int {
+	if right <= left {
+		return m
+	}
+
+	wall := left
+
+	for i := left; i <= right; i++ {
+		if m[right] >= m[i] {
+			if m[i] < m[wall] {
+				m[i], m[wall] = m[wall], m[i]
+			}
+			wall++
+		}
+	}
+
+	quickSort(m, left, wall-2)
+	quickSort(m, wall, right)
+
+	return m
+}
+
+func main() {
+	m := []int{7, 2, 5, 0, 1, 8, 3, 6, 9, 4}
+
+	fmt.Print(quickSort(m, 0, len(m)-1))
 }
